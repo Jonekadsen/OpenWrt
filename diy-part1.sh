6766593663e98eaadd7f687b41c69988fe513cdd
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2021-04-18 19:58:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,23 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+
+git clone https://github.com/jerrykuku/luci-app-vssr.git  #helloworld
+
+git clone https://github.com/fw876/helloworld  #ssrplus
+
+git clone https://github.com/xiaorouji/openwrt-passwall  #passswall
+
+git clone https://github.com/tty228/luci-app-serverchan  #微信推送
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome  #adguardhome
+
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus  #京东
+
+git clone https://github.com/pymumu/openwrt-smartdns  #smartdns
+
+git clon https://github.com/pymumu/luci-app-smartdns/tree/lede  #smartdns
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
