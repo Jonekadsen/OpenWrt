f7cf8644eb144b8e5c043e835802b9375af9d094
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2021-04-23 09:02:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,21 @@
 #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 #helloworld
-git clone https://github.com/jerrykuku/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #ssrplus
-git clone https://github.com/fw876/helloworld
+git clone https://github.com/fw876/helloworld package/helloworld
 #passswall
-git clone https://github.com/xiaorouji/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 #微信推送
-git clone https://github.com/tty228/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #京东
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 #smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
 #smartdns界面
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 
 # argon主题
 pushd package/lean
