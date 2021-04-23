b0286f9750757dba300db7bd4f9707dcb45e04b2
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2021-04-23 08:44:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,49 +1,26 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#git lua-maxminddb 依赖
+git clone https://github.com/jerrykuku/lua-maxminddb
+#helloworld
+git clone https://github.com/jerrykuku/luci-app-vssr
+#ssrplus
+git clone https://github.com/fw876/helloworld
+#passswall
+git clone https://github.com/xiaorouji/openwrt-passwall
+#微信推送
+git clone https://github.com/tty228/luci-app-serverchan
+#adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+#京东
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
+#smartdns
+git clone https://github.com/pymumu/openwrt-smartdns
+#smartdns界面
+git clone -b lede https://github.com/pymumu/luci-app-smartdns
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
-
-git clone https://github.com/jerrykuku/luci-app-vssr.git  #helloworld
-
-git clone https://github.com/fw876/helloworld  #ssrplus
-
-git clone https://github.com/xiaorouji/openwrt-passwall  #passswall
-
-git clone https://github.com/tty228/luci-app-serverchan  #微信推送
-
-git clone https://github.com/rufengsuixing/luci-app-adguardhome  #adguardhome
-
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus  #京东
-
-WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
-unzip $WORKINGDIR/master.zip -d $WORKINGDIR
-mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
-rmdir $WORKINGDIR/openwrt-smartdns-master
-rm $WORKINGDIR/master.zip
-
-LUCIBRANCH="lede" #更换此变量
-WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
-unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
-mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
-rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
-rm $WORKINGDIR/${LUCIBRANCH}.zip
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# argon主题
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+# Edge主题
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
