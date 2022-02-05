119e6fc42a1df841eb4521bd3895d5b0f05f5527
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2022-02-05 13:13:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,13 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 #京东
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 #smartdns
-git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+git clone https://github.com/pymumu/smartdns.git
 #smartdns界面
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#OpenClash
+git clone https://github.com/vernesong/OpenClash.git
+
+
 
 # argon主题
 pushd package/lean
