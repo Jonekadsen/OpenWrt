53dc0099c4366b881a1ad2defb3bc062959b2491
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2022-02-05 13:51:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP
+sed -i 's/10.10.10.2/192.168.50.5/g' package/base-files/files/bin/config_generate
