480655bcc34e8b814d977fe8d273d77e499e0ec0
Jonekadsen <62378255+Jonekadsen@users.noreply.github.com>
2023-01-28 02:45:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,11 @@
-#git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 #helloworld
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
 #ssrplus
 git clone https://github.com/fw876/helloworld package/helloworld
 #passswall
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-#微信推送
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-#京东
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 #smartdns
 git clone https://github.com/pymumu/smartdns.git
 #smartdns界面
